343a27a5043d89f6155fd7cb166be4a8638df832
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-06-24 00:22:41 +0100

    Created interface, input area created for user to input thier answers to the generated questions. Added sparta icon also.

--- main.py
+++ main.py
@@ -1,30 +1,54 @@
-# Shows user a question
-# User provides an answer as an input of what they think the answer should be
-# Shows user the prefered answer
+# --Things to add--
+# Colour scheme options at the side or bottom (Dark mode etc)
+# Image viewer for some answers to show visual aid for topics
+
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter.font import Font
 from topics import agile, data_consepts, database_design, sql, python
 
 
-def get_question():
-    return agile[1]
+class SpartaQuestionGenerator:
+    def __init__(self, root):
+        root.title("Sparta Question Generator")
+        root.resizable(False, False)
+        root.minsize(750, 550)
+        root.iconbitmap("assets\logo\logo.ico")
+
+        font_title = Font(
+            family="roboto",
+            size=20,
+            weight="normal",
+            slant="roman",
+            underline=0,
+            overstrike=0,
+        )
+
+        font_inp = Font(
+            family="roboto",
+            size=12,
+            weight="normal",
+            slant="roman",
+            underline=0,
+            overstrike=0,
+        )
+
+        # Outer frame
+        outer_frame = ttk.Frame(root, padding=".2i")
+        outer_frame.pack()
+
+        title = ttk.Label(outer_frame, text="Question Generator", font=font_title)
+        title.pack()
+
+        # Entry field for user answer
+        user_answer = tk.Text(outer_frame, width=100, height=10, font=font_inp)
+        user_answer.pack()
+
+        user_answer = ttk.Button(outer_frame, text="Compare Answer")
+        user_answer.pack()
 
 
-run = True
-qcnt = 1
-end_answer = True
-
-while run:
-
-    question = get_question()[0]
-    answer = get_question()[1]
-
-    print(f"\nQuestion {qcnt}: {question}\n")
-
-    while end_answer:
-        user_answer = input(" - ")
-        if user_answer == "":
-            end_answer = False
-
-    print(f"\n{answer}")
-
-    qcnt += 1
-    break
+root = tk.Tk()
+SpartaQuestionGenerator(root)
+root.mainloop()
