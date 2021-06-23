0a702e99aaca3112b5b4305cc46f34a0b15539ef
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-06-23 23:24:39 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,3 @@
+# Shows user a question
+# User provides an answer as an input of what they think the answer should be
+# Shows user the prefered answer
---
+++ topics.py
@@ -0,0 +1,50 @@
+agile = {
+    1: [
+        "What are the SCRUM events?",
+        "Sprint Planning (Product Backlog, Sprint goal progress, Sprint Backlog)\nDaily Scrum (Sprint Backlog, Daily plan)\nSprint Review (Product backlog, Product increment)\nSprint Retrospective (Definition of Done, Improvements, Team Collaboration)",
+    ],
+    2: ["What are the scrum artifacts?", "A"],
+    3: [
+        "What is your understanding of Agile delivery? What are the benefits and drawbacks?"
+    ],
+    4: ["How does Agile compare with other methodologies like waterfall?"],
+    5: ["Are there any drawbacks to Agile? "],
+    6: [
+        "In what situations would another methodology work better over Agile? Can you give an example?"
+    ],
+    7: ["What are user stories?"],
+    8: ["How can we prioritise our product backlog?"],
+    9: ["What information radiators can we use during retrospectives?"],
+    10: ["What is the purpose of a review?"],
+    11: ["What is the definition of ready?"],
+    12: ["What is a definition of done?"],
+    13: ["Why is it important to have good acceptance criteria for user stories?"],
+    14: ["What is the INVEST criteria for good user stories?"],
+    15: ["Why is it a good idea to use the INVEST criteria?"],
+    16: ["What do you need to consider when gathering requirements?"],
+}
+
+data_consepts = {
+    1: [],
+    3: [],
+    2: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+}
+
+database_design = {}
+
+sql = {}
+
+python = {}
+
+if __name__ == "__main__":
+    print("")
+    category = agile
+    num = 1
+    print(f"Question: {category[num][0]}\n\nAnswer:\n{category[num][1]}")
+
+    print("")
